e3c33afc2819f85acd6f7a6fabeae88524739218
Ahmed Ashraf <ahmed29329@gmail.com>
2016-07-01 22:21:22 +0200

    add php mbstring extenison

--- ubuntu.sh
+++ ubuntu.sh
@@ -58,13 +58,13 @@ echo "INSTALLING PHP Important Extensions"
 echo "##############"
 echo " "
 
-sudo apt-get install php7.0-mbstring
 sudo apt-get install php7.0-mcrypt
 sudo apt-get install php7.0-curl
 sudo apt-get install php7.0-zip
 sudo apt-get install php7.0-dom
 sudo apt-get install php7.0-gd
 sudo apt-get install php7.0-intl
+sudo apt-get install php7.0-mbstring
 
 
 echo " "
